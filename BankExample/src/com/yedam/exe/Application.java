4aea25ff6d65529c666caf643061b6b0680851d4
admin <admin@YD02-02>
2023-05-19 17:51:05 +0900

    project practice

---
+++ BankExample/src/com/yedam/exe/Application.java
@@ -0,0 +1,43 @@
+package com.yedam.exe;
+
+import java.util.Scanner;
+
+import com.yedam.member.MemberService;
+
+public class Application {
+
+	Scanner sc = new Scanner(System.in);
+	MemberService ms = new MemberService();
+	
+	public Application() {
+		run();
+	}
+
+	private void run() {
+		while(true) {
+		System.out.println("1. 로그인 | 2. 종료");
+		int menu = Integer.parseInt(sc.nextLine());
+		if (menu == 1) {
+			// 로그인
+			if (MemberService.memberInfo == null) {
+				ms.login();
+			} else {
+				// 로그인한 정보를 토대로 고객 / 은행원의 업무를 나눔
+				if (MemberService.memberInfo.getMemberAuth().equals("N")) {
+					new MemberApp();
+				} else if (MemberService.memberInfo.getMemberAuth().equals("A")) {
+					new AccountApp();
+				}
+			}
+		} else if (menu == 2) {
+			System.out.println("은행 업무 종료");
+			break;
+		} else {
+			System.out.println("잘못 입력");
+		}
+		
+		}
+		
+	}
+	
+}
---
+++ BankExample/src/com/yedam/member/MemberService.java
@@ -0,0 +1,30 @@
+package com.yedam.member;
+
+import java.util.Scanner;
+
+public class MemberService {
+	
+	public static Member memberInfo = null;
+	Scanner sc = new Scanner(System.in);
+	
+	public void login() {
+		System.out.println("로그인✨");
+		System.out.println("ID>");
+		String id = sc.nextLine();
+		System.out.println("PW>");
+		String pw = sc.nextLine();
+		
+		Member member = MemberDAO.getInstance().login(id);
+		
+		if (member != null) {
+			if (member.getMemberPw().equals(pw)) {
+				System.out.println("로그인 성공");
+				} else {
+				System.out.println("비밀번호 불일치😢");
+			} 
+		} else {
+			System.out.println("아이디 불일치😢");
+		}
+		
+	}
+}
